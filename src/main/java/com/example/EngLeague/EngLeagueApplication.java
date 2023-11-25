429865d41cd46e6710335f43d4619b805d83b8d8
LBW315 <big.west.win.sentence.14@gmail.com>
2023-11-25 10:56:22 +0900

    first commit

---
+++ src/main/java/com/example/EngLeague/EngLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.example.EngLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EngLeague/EngLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EngLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
