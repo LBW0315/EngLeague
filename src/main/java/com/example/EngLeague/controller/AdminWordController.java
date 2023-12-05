b37da3dfe28e1f6ae8c723ed4caf921f582e9f71
LBW315 <big.west.win.sentence.14@gmail.com>
2023-12-05 22:42:35 +0900

    前回のエラーを解決し、7章をもとに進めたが再びエラー

--- src/main/java/com/example/EngLeague/controller/AdminWordController.java
+++ src/main/java/com/example/EngLeague/controller/AdminWordController.java
@@ -1,15 +1,18 @@
 package com.example.EngLeague.controller;
 
-import org.hibernate.mapping.List;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.EngLeague.entity.Word;
 import com.example.EngLeague.repository.WordRepository;
 
-import ch.qos.logback.core.model.Model;
+
 
 @Controller
 @RequestMapping("/admin/word")
@@ -25,7 +28,7 @@ public class AdminWordController {
 	public String index(Model model) {
 		List<Word> word = wordRepository.findAll();
 		
-		model.addAttribute("wd",word);
+		model.addAttribute("word",word);
 		
 		return "admin/word/index";
 	}
