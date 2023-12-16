89fbe210c133a393954ea75d0e6b7e5146021a31
LBW315 <big.west.win.sentence.14@gmail.com>
2023-12-16 16:34:30 +0900

    Unit検索の実装

--- src/main/java/com/example/EngLeague/controller/AdminWordController.java
+++ src/main/java/com/example/EngLeague/controller/AdminWordController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.EngLeague.entity.Word;
 import com.example.EngLeague.repository.WordRepository;
@@ -29,10 +30,18 @@ public class AdminWordController {
 	
 	
 	@GetMapping
-	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {	
-        Page<Word> wordPage = wordRepository.findAll(pageable);
+	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name = "unit", required = false)String unit) {	
+        Page<Word> wordPage;
+        
+        
+        if(unit != null && !unit.isEmpty()) {
+        	wordPage = wordRepository.findByUnitLike("%" + unit + "%", pageable);
+        }else {
+        	wordPage = wordRepository.findAll(pageable);
+        }
                 
         model.addAttribute("wordPage", wordPage);
+        model.addAttribute("unit", unit);
         
         return "admin/word/index";
     }  
--- src/main/java/com/example/EngLeague/repository/WordRepository.java
+++ src/main/java/com/example/EngLeague/repository/WordRepository.java
@@ -1,9 +1,11 @@
 package com.example.EngLeague.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.EngLeague.entity.Word;
 
 public interface WordRepository extends JpaRepository<Word, Integer> {
-
+	public Page<Word> findByUnitLike(String unit, Pageable pageable);
 }
